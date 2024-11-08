8d1cc264258b7b2bf5ec2c4caa9658904857f2d8
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-11-08 00:54:44 -0800

    l16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 // todo
+import "./style.css";
+
+const APP_NAME = "Geocoin";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+
+const button = document.createElement("button");
+button.textContent = "Click Me";
+app.appendChild(button);
+button.addEventListener("click", function () {
+  alert("you clicked the button!");
+});
