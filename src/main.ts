121439364e4ca815856a2b1d87e0f4c4003bcc28
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-11-26 10:44:07 -0800

    D3a complete

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 // todo
 import "./style.css";
+import leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./leafletWorkaround.ts";
 
 const APP_NAME = "Geocoin";
-const app = document.querySelector<HTMLDivElement>("#app")!;
-document.title = APP_NAME;
+const app: HTMLDivElement = document.querySelector("#app")!;
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
 
-const button = document.createElement("button");
-button.textContent = "Click Me";
-app.appendChild(button);
-button.addEventListener("click", function () {
-  alert("You clicked the button!");
+//Create map of Oake's College
+const map = leaflet.map("map", { zoomControl: false }).setView([
+  36.9895,
+  -122.06278,
+], 19);
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  minZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+//Set player location in Oakes College classroom
+const player = leaflet.marker([36.9895, -122.06278]).addTo(map);
+player.bindTooltip("You are here", {
+  permanent: false,
+  direction: "center",
 });
